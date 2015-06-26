bdf5734c5e04a0e490defd34c62f7c0838441a47
Jason Felds <jafelds@gmail.com>
2015-06-25 22:56:39 -0400

    Quick way to draw any surface with blitzing.

--- src/cSurface.cpp
+++ src/cSurface.cpp
@@ -12,3 +12,16 @@ std::unique_ptr<SDL_Surface, void(*)(SDL_Surface *)> CSurface::OnLoad(std::strin
   tmp.reset(SDL_LoadBMP(file.c_str()));
   return tmp;
 }
+
+bool CSurface::OnDraw(SDL_Surface * surfDest, SDL_Surface * surfSrc, int x, int y) {
+  if (surfSrc == nullptr || surfDest == nullptr) {
+    return false;
+  }
+  
+  SDL_Rect dest;
+  dest.x = x;
+  dest.y = y;
+  SDL_BlitSurface(surfSrc, nullptr, surfDest, &dest);
+  
+  return true;
+}
--- src/cSurface.hpp
+++ src/cSurface.hpp
@@ -10,6 +10,8 @@ public:
   CSurface();
   
   static std::unique_ptr<SDL_Surface, void(*)(SDL_Surface *)> OnLoad(std::string file);
+  
+  static bool OnDraw(SDL_Surface * surfDest, SDL_Surface * surfSrc, int x, int y);
 };
 
 #endif
